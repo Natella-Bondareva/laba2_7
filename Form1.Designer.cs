a1ebc6144bfa87206d51e4e4374d2671be3da842
natella <natellabondareva20@gmail.com>
2023-06-05 13:20:43 +0300

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,146 @@
+﻿namespace laba2_7
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.TaskA = new System.Windows.Forms.Button();
+            this.TaskB = new System.Windows.Forms.Button();
+            this.TaskC = new System.Windows.Forms.Button();
+            this.TaskD = new System.Windows.Forms.Button();
+            this.Close = new System.Windows.Forms.Button();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.timer2 = new System.Windows.Forms.Timer(this.components);
+            this.pic = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pic)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // TaskA
+            // 
+            this.TaskA.BackColor = System.Drawing.SystemColors.Control;
+            this.TaskA.Location = new System.Drawing.Point(30, 391);
+            this.TaskA.Name = "TaskA";
+            this.TaskA.Size = new System.Drawing.Size(75, 23);
+            this.TaskA.TabIndex = 0;
+            this.TaskA.Text = "Task a";
+            this.TaskA.UseVisualStyleBackColor = false;
+            this.TaskA.Click += new System.EventHandler(this.TaskA_Click);
+            // 
+            // TaskB
+            // 
+            this.TaskB.BackColor = System.Drawing.SystemColors.Control;
+            this.TaskB.Location = new System.Drawing.Point(158, 391);
+            this.TaskB.Name = "TaskB";
+            this.TaskB.Size = new System.Drawing.Size(75, 23);
+            this.TaskB.TabIndex = 1;
+            this.TaskB.Text = "Task b";
+            this.TaskB.UseVisualStyleBackColor = false;
+            this.TaskB.Click += new System.EventHandler(this.TaskB_Click);
+            // 
+            // TaskC
+            // 
+            this.TaskC.BackColor = System.Drawing.SystemColors.Control;
+            this.TaskC.Location = new System.Drawing.Point(279, 391);
+            this.TaskC.Name = "TaskC";
+            this.TaskC.Size = new System.Drawing.Size(75, 23);
+            this.TaskC.TabIndex = 2;
+            this.TaskC.Text = "Task c";
+            this.TaskC.UseVisualStyleBackColor = false;
+            this.TaskC.Click += new System.EventHandler(this.TaskC_Click);
+            // 
+            // TaskD
+            // 
+            this.TaskD.BackColor = System.Drawing.SystemColors.Control;
+            this.TaskD.Location = new System.Drawing.Point(403, 391);
+            this.TaskD.Name = "TaskD";
+            this.TaskD.Size = new System.Drawing.Size(75, 23);
+            this.TaskD.TabIndex = 3;
+            this.TaskD.Text = "Task d";
+            this.TaskD.UseVisualStyleBackColor = false;
+            this.TaskD.Click += new System.EventHandler(this.TaskD_Click);
+            // 
+            // Close
+            // 
+            this.Close.BackColor = System.Drawing.SystemColors.Control;
+            this.Close.Location = new System.Drawing.Point(682, 391);
+            this.Close.Name = "Close";
+            this.Close.Size = new System.Drawing.Size(75, 23);
+            this.Close.TabIndex = 4;
+            this.Close.Text = "Close";
+            this.Close.UseVisualStyleBackColor = false;
+            this.Close.Click += new System.EventHandler(this.Close_Click);
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // timer2
+            // 
+            this.timer2.Tick += new System.EventHandler(this.timer2_Tick);
+            // 
+            // pic
+            // 
+            this.pic.Location = new System.Drawing.Point(64, 29);
+            this.pic.Name = "pic";
+            this.pic.Size = new System.Drawing.Size(596, 335);
+            this.pic.TabIndex = 5;
+            this.pic.TabStop = false;
+            this.pic.Visible = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.pic);
+            this.Controls.Add(this.Close);
+            this.Controls.Add(this.TaskD);
+            this.Controls.Add(this.TaskC);
+            this.Controls.Add(this.TaskB);
+            this.Controls.Add(this.TaskA);
+            this.DoubleBuffered = true;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pic)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button TaskA;
+        private System.Windows.Forms.Button TaskB;
+        private System.Windows.Forms.Button TaskC;
+        private System.Windows.Forms.Button TaskD;
+        private System.Windows.Forms.Button Close;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Timer timer2;
+        private System.Windows.Forms.PictureBox pic;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,143 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace laba2_7
+{
+    public partial class Form1 : Form
+    {
+        Image[] frogFrames;
+        public Form1()
+        {
+            InitializeComponent();
+
+            frogFrames = new Image[18];
+            frogFrames[0] = Image.FromFile("frog1.png");
+            frogFrames[1] = Image.FromFile("frog2.png");
+            frogFrames[2] = Image.FromFile("frog3.png");
+            frogFrames[3] = Image.FromFile("frog4.png");
+            frogFrames[4] = Image.FromFile("frog5.png");
+            frogFrames[5] = Image.FromFile("frog6.png");
+            frogFrames[6] = Image.FromFile("frog7.png");
+            frogFrames[7] = Image.FromFile("frog8.png");
+            frogFrames[8] = Image.FromFile("frog9.png");
+            frogFrames[9] = Image.FromFile("frog10.png");
+            frogFrames[10] = Image.FromFile("frog11.png");
+            frogFrames[11] = Image.FromFile("frog12.png");
+            frogFrames[12] = Image.FromFile("frog13.png");
+            frogFrames[13] = Image.FromFile("frog14.png");
+            frogFrames[14] = Image.FromFile("frog15.png");
+            frogFrames[15] = Image.FromFile("frog16.png");
+            frogFrames[16] = Image.FromFile("frog17.png");
+            frogFrames[17] = Image.FromFile("frog18.png");
+        }
+        Graphics g;
+        private void TaskA_Click(object sender, EventArgs e)
+        {
+            g = CreateGraphics();
+            Point[] trianglePoints = { new Point(100, 100), new Point(100, 200), new Point(200, 200), new Point(100, 100) };
+            g.DrawLines(Pens.Black, trianglePoints);
+            g.DrawRectangle(Pens.Red, 260,150, 100,100);
+
+            Point[] starPoints = new Point[10];
+            double outerRadius = 60; 
+            double innerRadius = outerRadius / 2.5; 
+
+            for (int i = 0; i < 10; i++)
+            {
+                double angle = - Math.PI / 6 + i * Math.PI / 5;
+                double radius = i % 2 == 0 ? outerRadius : innerRadius;
+                int x = (int)(Math.Cos(angle) * radius)+250;
+                int y = (int)(Math.Sin(angle) * radius)+250;
+                starPoints[i] = new Point(x, y);
+            }
+
+            g.FillPolygon(Brushes.Yellow, starPoints);
+            g.DrawPolygon(Pens.YellowGreen, starPoints);
+
+            g.DrawEllipse(Pens.Purple, 350, 120, 200, 100);
+        }
+        private void TaskB_Click(object sender, EventArgs e)
+        {
+            g = CreateGraphics();
+            Pen pen1 = new Pen(Color.Black, 8);
+            g.DrawEllipse(pen1, 350, 200, 100, 100);
+            g.DrawEllipse(pen1, 150, 200, 100, 100);
+            Pen pen2 = new Pen(Color.DarkCyan, 8);
+            Point[] points = { new Point(400, 250), new Point(370, 190), new Point(300, 250), new Point(200, 250), new Point(260, 190), new Point(370, 190), new Point(300, 250), new Point(245, 170) };
+            g.DrawLines(pen2, points);
+            g.DrawLine(pen1, new Point(225, 170), new Point(265, 170));
+            g.DrawLine(pen2, new Point(370, 190), new Point(370, 170));
+            g.DrawLine(pen1, new Point(370, 170), new Point(340, 170));
+        }
+        private void TaskC_Click(object sender, EventArgs e)
+        {
+            timer1.Start();
+        }
+        int step = 0;
+        Point a = new Point(275, 125);
+        Point b = new Point(275, 75);
+        Point c = new Point(325, 75);
+        Point d = new Point(325, 125);
+        Point cente = new Point(300, 175);
+        Point[] newPoints;
+        Point newA, newB, newC, newD;
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            g = CreateGraphics();
+            g.Clear(BackColor);
+            g.DrawEllipse(Pens.Black,250,125,100,100);
+            double angle = step * Math.PI / 100;
+
+            newA.X = (int)((a.X - cente.X) * Math.Cos(angle) - (a.Y - cente.Y) * Math.Sin(angle) + cente.X);
+            newA.Y = (int)((a.X - cente.X) * Math.Sin(angle) + (a.Y - cente.Y) * Math.Cos(angle) + cente.Y);
+            newB.X = (int)((b.X - cente.X) * Math.Cos(angle) - (b.Y - cente.Y) * Math.Sin(angle) + cente.X);
+            newB.Y = (int)((b.X - cente.X) * Math.Sin(angle) + (b.Y - cente.Y) * Math.Cos(angle) + cente.Y);
+            newC.X = (int)((c.X - cente.X) * Math.Cos(angle) - (c.Y - cente.Y) * Math.Sin(angle) + cente.X);
+            newC.Y = (int)((c.X - cente.X) * Math.Sin(angle) + (c.Y - cente.Y) * Math.Cos(angle) + cente.Y);
+            newD.X = (int)((d.X - cente.X) * Math.Cos(angle) - (d.Y - cente.Y) * Math.Sin(angle) + cente.X);
+            newD.Y = (int)((d.X - cente.X) * Math.Sin(angle) + (d.Y - cente.Y) * Math.Cos(angle) + cente.Y);
+
+            Point[] newPoints = { newA, newB, newC, newD, newA};
+            g.DrawLines(Pens.Gray, newPoints);
+            step++;
+        }
+        private void TaskD_Click(object sender, EventArgs e)
+        {
+            pic.Visible = true;
+            pic.SizeMode = PictureBoxSizeMode.StretchImage;
+            timer2.Start();
+
+        }
+        int curr = 0;
+        private void timer2_Tick(object sender, EventArgs e)
+        {
+            curr++;
+            if (curr >= frogFrames.Length)
+                curr = 0;
+
+            pic.Image = frogFrames[curr];
+        }
+        private void Close_Click(object sender, EventArgs e)
+        {
+            step = 0;
+            timer1.Stop();
+            timer2.Stop();
+            pic.Visible = false;
+            try
+            {
+                g.Clear(BackColor);
+            }
+            catch (System.NullReferenceException)
+            {
+
+            }
+        }
+    }
+}
